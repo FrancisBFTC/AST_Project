ff45364781dc6c8eb88c4df117a013f1744cdcdf
FrancisBFTC <bftcorporations@gmail.com>
2026-02-03 23:49:14 -0300

    feat: Create AST Repository

---
+++ src/ast.c
@@ -0,0 +1,8 @@
+#include "astlib.h"
+
+int main(int argc, char *argv[])
+{
+  
+  system("PAUSE");	
+  return 0;
+}
---
+++ src/astlib.h
@@ -0,0 +1,15 @@
+#ifndef _ASTLIB_H_
+#define _ASTLIB_H_
+
+#ifndef _INC_STDIO
+#include <stdio.h>
+#endif
+#ifndef _INC_STDLIB
+#include <stdlib.h>
+#endif
+
+
+
+
+
+#endif
