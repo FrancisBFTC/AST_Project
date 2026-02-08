ef78702a436f2bf2d7d7984deed9dbcd96535513
FrancisBFTC <bftcorporations@gmail.com>
2026-02-08 03:54:15 -0300

    AST finished

--- src/ast.c
+++ src/ast.c
@@ -4,6 +4,8 @@ int main(int argc, char *argv[])
 {
 	bool state = false;
 	AST *tree = parse("(281 - 394 + 53 + (9 - 7 * 3) - 1) / 2");
-	printf("Resultado: %d\n", eval(tree, &state)); // -36	
+	printf("Resultado: %d\n", eval(tree, &state)); // -36
+	free_ast(tree);
+	
 	return 0;
 }
